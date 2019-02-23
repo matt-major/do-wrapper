b1b41c783adacf562bf938b1af7acc6528207f7b
Satya Rohith <gsatyarohith@gmail.com>
2019-02-23 14:54:07 +0530

    feat(db): add databasesGetPool method

--- src/do-wrapper.js
+++ src/do-wrapper.js
@@ -2120,6 +2120,21 @@ export default class DigitalOcean {
   }
 
   /**
+   * Get information about an existing connection pool
+   * Info: {@link https://developers.digitalocean.com/documentation/v2/#retrieve-an-existing-connection-pool  retrieve-an-existing-connection-pool}
+   * @param {string} clusterId - The database cluster ID
+   * @param {string} poolName - The name of the pool
+   * @param {*} callback - Optional function to execute on completion
+   * @returns {Promise|undefined} - Returns a promise if [callback] is not defined
+   */
+  databasesGetPool(clusterId, poolName, callback) {
+    const options = {
+      actionPath: `databases/${encodeURIComponent(clusterId)}/pools/${encodeURIComponent(poolName)}`,
+    };
+    return this._handleRequest(options, callback);
+  }
+
+  /**
    * Get All Kubernetes Clusters
    * Info: {@link https://developers.digitalocean.com/documentation/v2/#list-all-kubernetes-clusters kubernetes-get-clusters}
    * @param {object} [query] - Optional query parameters
