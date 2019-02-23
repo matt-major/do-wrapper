c0f40817198d6e379e51735a8f20de82aee6dc20
Satya Rohith <gsatyarohith@gmail.com>
2019-02-23 12:37:33 +0530

    feat(db): add databasesUsersGetAll method

--- src/do-wrapper.js
+++ src/do-wrapper.js
@@ -1995,6 +1995,20 @@ export default class DigitalOcean {
   }
 
   /**
+   * List all the users of a database cluster
+   * Info: {@link https://developers.digitalocean.com/documentation/v2/#list-all-database-users list-all-database-users}
+   * @param {string} clusterId - The database cluster ID
+   * @param {*} callback - Optional function to execute on completion
+   * @returns {Promise|undefined} - Returns a promise if [callback] is not defined
+   */
+  databasesUsersGetAll(clusterId, callback) {
+    const options = {
+      actionPath: `databases/${encodeURIComponent(clusterId)}/users`
+    };
+    return this._handleRequest(options, callback);
+  }
+
+  /**
    * Get All Kubernetes Clusters
    * Info: {@link https://developers.digitalocean.com/documentation/v2/#list-all-kubernetes-clusters kubernetes-get-clusters}
    * @param {object} [query] - Optional query parameters
