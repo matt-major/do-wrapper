38f24fa882f73073cfb0d335b1690a2ea4f38223
matt-major <mattmajor14@gmail.com>
2019-09-20 18:01:17 +0100

    Add CDN Endpoint methods

--- dist/modules/cdn.js
+++ dist/modules/cdn.js
@@ -17,8 +17,106 @@ var base_module_1 = require("./base-module");
 var CDN = /** @class */ (function (_super) {
     __extends(CDN, _super);
     function CDN(pageSize, requestHelper) {
-        return _super.call(this, pageSize, requestHelper) || this;
+        var _this = _super.call(this, pageSize, requestHelper) || this;
+        _this.basePath = 'cdn/endpoints';
+        _this.baseOptions = {
+            actionPath: _this.basePath + "/",
+        };
+        return _this;
     }
+    /**
+     * Get all CDN Endpoints
+     * @param tagName filter to only return CDN Endpoints with a given tag
+     * @param [includeAll] return all CDN Endpoints, paginated (optional)
+     * @param [page] the specific page of CDN Endpoints to return (optional)
+     * @param [pageSize] the number of CDN Endpoints to return per page (optional)
+     * @returns Promise
+     */
+    CDN.prototype.getAllEndpoints = function (tagName, includeAll, page, pageSize) {
+        if (includeAll === void 0) { includeAll = false; }
+        if (page === void 0) { page = 1; }
+        if (pageSize === void 0) { pageSize = this.pageSize; }
+        var requestOptions = this._getBasePaginatedRequestOptions({
+            actionPath: this.basePath,
+            key: 'endpoints',
+            tagName: tagName,
+            pageSize: pageSize,
+            page: page,
+            includeAll: includeAll,
+        });
+        return this._execute(requestOptions);
+    };
+    /**
+     * Get a CDN Endpoint via its Id
+     *
+     * @param endpointId the identifier of the endpoint to retrieve
+     * @returns Promise
+     */
+    CDN.prototype.getEndpointById = function (endpointId) {
+        return this._execute({
+            actionPath: this.basePath + "/" + encodeURIComponent(endpointId),
+        });
+    };
+    /**
+     * Create a new CDN Endpoint
+     *
+     * @param origin The origin server address (FQDN) which provides the content for the CDN
+     * @param ttl The amount of time (seconds) the content is cached by the CDN's edge servers. Defaults to 3600 (one hour)
+     * @returns Promise
+     */
+    CDN.prototype.createEndpoint = function (origin, ttl) {
+        return this._execute({
+            actionPath: this.basePath,
+            method: 'POST',
+            body: {
+                origin: origin,
+                ttl: ttl,
+            },
+        });
+    };
+    /**
+     * Update the TTL of an existing CDN Endpoint
+     *
+     * @param endpointId the identifier of the CDN Endpoint you wish to update
+     * @param ttl the amount of time (seconds) the content is cached by the CDN's edge servers
+     * @returns Promise
+     */
+    CDN.prototype.updateEndpointTtl = function (endpointId, ttl) {
+        return this._execute({
+            actionPath: this.basePath + "/" + encodeURIComponent(endpointId),
+            method: 'PUT',
+            body: {
+                ttl: ttl,
+            },
+        });
+    };
+    /**
+     * Delete an existing CDN Endpoint
+     *
+     * @param endpointId the identifier of the CDN Endpoint you wish to update
+     */
+    CDN.prototype.deleteEndpoint = function (endpointId) {
+        return this._execute({
+            actionPath: this.basePath + "/" + encodeURIComponent(endpointId),
+            method: 'DELETE',
+        });
+    };
+    /**
+     * Purge cached content from an existing CDN Endpoint
+     *
+     * @param endpointId the identifier of the CDN Endpoint to purge the cache on
+     * @param fileNames an array of path strings to clear purge from the cache
+     * @returns Promise
+     */
+    CDN.prototype.purgeEndpointCache = function (endpointId, fileNames) {
+        return this._execute({
+            actionPath: this.basePath + "/" + encodeURIComponent(endpointId) + "/cache",
+            method: 'DELETE',
+            body: {
+                files: fileNames,
+            },
+        });
+    };
     return CDN;
 }(base_module_1.BaseModule));
 exports.default = CDN;
--- dist/modules/snapshots.js
+++ dist/modules/snapshots.js
@@ -12,13 +12,100 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var base_module_1 = require("./base-module");
 var Snapshots = /** @class */ (function (_super) {
     __extends(Snapshots, _super);
     function Snapshots(pageSize, requestHelper) {
-        return _super.call(this, pageSize, requestHelper) || this;
+        var _this = _super.call(this, pageSize, requestHelper) || this;
+        _this.basePath = 'snapshots';
+        _this.baseOptions = {
+            actionPath: _this.basePath,
+            key: 'snapshots',
+        };
+        return _this;
     }
+    /**
+     * Get all Snapshots, regardless of type
+     * @param tagName filter to only return Snapshots with a given tag
+     * @param [includeAll] return all Snapshots, paginated (optional)
+     * @param [page] the specific page of Snapshots to return (optional)
+     * @param [pageSize] the number of Snapshots to return per page (optional)
+     * @returns Promise
+     */
+    Snapshots.prototype.get = function (tagName, includeAll, page, pageSize) {
+        if (includeAll === void 0) { includeAll = false; }
+        if (page === void 0) { page = 1; }
+        if (pageSize === void 0) { pageSize = this.pageSize; }
+        var requestOptions = this._getBasePaginatedRequestOptions(__assign({}, this.baseOptions, { tagName: tagName, pageSize: pageSize, page: page, includeAll: includeAll }));
+        return this._execute(requestOptions);
+    };
+    /**
+     * Get a Snapshot using its identifier
+     * @param snapshotId the identifier of the Snapshot to retrieve
+     * @returns Promise
+     */
+    Snapshots.prototype.getById = function (snapshotId) {
+        return this._execute({
+            actionPath: this.basePath + "/" + encodeURIComponent(snapshotId),
+        });
+    };
+    /**
+     * Get all Snapshots, only for Volumes
+     * @param tagName filter to only return Snapshots with a given tag
+     * @param [includeAll] return all Snapshots, paginated (optional)
+     * @param [page] the specific page of Snapshots to return (optional)
+     * @param [pageSize] the number of Snapshots to return per page (optional)
+     * @returns Promise
+     */
+    Snapshots.prototype.getForVolumes = function (tagName, includeAll, page, pageSize) {
+        if (includeAll === void 0) { includeAll = false; }
+        if (page === void 0) { page = 1; }
+        if (pageSize === void 0) { pageSize = this.pageSize; }
+        var requestOptions = this._getBasePaginatedRequestOptions(__assign({}, this.baseOptions, { tagName: tagName, pageSize: pageSize, page: page, includeAll: includeAll, qs: {
+                resource_type: 'volume',
+            } }));
+        return this._execute(requestOptions);
+    };
+    /**
+     * Get all Snapshots, only for Droplets
+     * @param tagName filter to only return Snapshots with a given tag
+     * @param [includeAll] return all Snapshots, paginated (optional)
+     * @param [page] the specific page of Snapshots to return (optional)
+     * @param [pageSize] the number of Snapshots to return per page (optional)
+     * @returns Promise
+     */
+    Snapshots.prototype.getForDroplets = function (tagName, includeAll, page, pageSize) {
+        if (includeAll === void 0) { includeAll = false; }
+        if (page === void 0) { page = 1; }
+        if (pageSize === void 0) { pageSize = this.pageSize; }
+        var requestOptions = this._getBasePaginatedRequestOptions(__assign({}, this.baseOptions, { tagName: tagName, pageSize: pageSize, page: page, includeAll: includeAll, qs: {
+                resource_type: 'droplet',
+            } }));
+        return this._execute(requestOptions);
+    };
+    /**
+     * Delete a Snapshot
+     * @param snapshotId the identifier of the Snapshot to delete
+     * @returns Promise
+     */
+    Snapshots.prototype.deleteById = function (snapshotId) {
+        return this._execute({
+            method: 'DELETE',
+            actionPath: this.basePath + "/" + encodeURIComponent(snapshotId),
+        });
+    };
     return Snapshots;
 }(base_module_1.BaseModule));
 exports.default = Snapshots;
