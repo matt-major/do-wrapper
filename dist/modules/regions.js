7807f89eeacf8ccff435ad200e412f39056e9447
matt-major <mattmajor14@gmail.com>
2019-10-12 20:19:15 +0100

    Regions

--- dist/modules/regions.js
+++ dist/modules/regions.js
@@ -17,8 +17,35 @@ var base_module_1 = require("./base-module");
 var Regions = /** @class */ (function (_super) {
     __extends(Regions, _super);
     function Regions(pageSize, requestHelper) {
-        return _super.call(this, pageSize, requestHelper) || this;
+        var _this = _super.call(this, pageSize, requestHelper) || this;
+        _this.basePath = 'regions';
+        _this.baseOptions = {
+            actionPath: _this.basePath + "/",
+        };
+        return _this;
     }
+    /**
+     * Get all Regions
+     * @param tagName filter to only return Regions with a given tag
+     * @param [includeAll] return all Regions, paginated (optional)
+     * @param [page] the specific page of Regions to return (optional)
+     * @param [pageSize] the number of Regions to return per page (optional)
+     * @returns Promise
+     */
+    Regions.prototype.get = function (tagName, includeAll, page, pageSize) {
+        if (includeAll === void 0) { includeAll = false; }
+        if (page === void 0) { page = 1; }
+        if (pageSize === void 0) { pageSize = this.pageSize; }
+        var requestOptions = this._getBasePaginatedRequestOptions({
+            actionPath: this.basePath,
+            key: 'regions',
+            tagName: tagName,
+            pageSize: pageSize,
+            page: page,
+            includeAll: includeAll,
+        });
+        return this._execute(requestOptions);
+    };
     return Regions;
 }(base_module_1.BaseModule));
 exports.default = Regions;
--- src/modules/regions.ts
+++ src/modules/regions.ts
@@ -2,7 +2,34 @@ import RequestHelper from "../request-helper";
 import {BaseModule} from "./base-module";
 
 export default class Regions extends BaseModule {
+    private basePath: string = 'regions';
+
+    private baseOptions: any = {
+        actionPath: `${this.basePath}/`,
+    };
+
     constructor(pageSize: number, requestHelper: RequestHelper) {
         super(pageSize, requestHelper);
+    }
+
+    /**
+     * Get all Regions
+     * @param tagName filter to only return Regions with a given tag
+     * @param [includeAll] return all Regions, paginated (optional)
+     * @param [page] the specific page of Regions to return (optional)
+     * @param [pageSize] the number of Regions to return per page (optional)
+     * @returns Promise
+     */
+    public get(tagName: string, includeAll: boolean = false, page: number = 1, pageSize: number = this.pageSize): Promise<any> {
+        const requestOptions = this._getBasePaginatedRequestOptions({
+            actionPath: this.basePath,
+            key: 'regions',
+            tagName: tagName,
+            pageSize: pageSize,
+            page: page,
+            includeAll: includeAll,
+        });
+
+        return this._execute(requestOptions);
     }
 }
