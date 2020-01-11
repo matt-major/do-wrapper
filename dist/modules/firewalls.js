95e0b2a638386f617fe6e97ee3143395245faaad
matt-major <mattmajor14@gmail.com>
2020-01-11 19:14:41 +0000

    Add Firewall methods

--- dist/modules/firewalls.js
+++ dist/modules/firewalls.js
@@ -12,13 +12,158 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var base_module_1 = require("./base-module");
+var common_1 = require("../common");
 var Firewalls = /** @class */ (function (_super) {
     __extends(Firewalls, _super);
     function Firewalls(pageSize, requestHelper) {
-        return _super.call(this, pageSize, requestHelper) || this;
+        var _this = _super.call(this, pageSize, requestHelper) || this;
+        _this.basePath = 'firewalls';
+        _this.baseOptions = {
+            actionPath: _this.basePath + "/",
+        };
+        return _this;
     }
+    /**
+     * Create a new Firewall
+     * @param options the options for the new Firewall
+     * @returns Promise
+     */
+    Firewalls.prototype.create = function (options) {
+        return this._execute(__assign({}, this.baseOptions, { method: common_1.HttpMethods.POST, body: options }));
+    };
+    /**
+     * Delete a Firewall by its identifier
+     * @param firewallId the identifier of the Firewall to delete
+     * @returns Promise
+     */
+    Firewalls.prototype.delete = function (firewallId) {
+        return this._execute({
+            actionPath: this.basePath + "/" + encodeURIComponent(firewallId),
+            method: common_1.HttpMethods.DELETE,
+        });
+    };
+    /**
+     * Retrieve a Firewall using its identifier
+     * @param firewallId the identifier of the Firewall to retrieve
+     * @returns Promise
+     */
+    Firewalls.prototype.getById = function (firewallId) {
+        return this._execute({
+            actionPath: this.basePath + "/" + encodeURIComponent(firewallId),
+        });
+    };
+    /**
+     * Update a Firewall using its identifier
+     * @param firewallId the identifier of the Firewall to update
+     * @param options the Firewall details
+     * @returns Promise
+     */
+    Firewalls.prototype.update = function (firewallId, options) {
+        return this._execute({
+            actionPath: this.basePath + "/" + encodeURIComponent(firewallId),
+            method: common_1.HttpMethods.PUT,
+            body: options,
+        });
+    };
+    /**
+     * Add Droplets to an existing Firewall
+     * @param firewallId the identifier of the Firewall to add the Droplets to
+     * @param dropletIds the identifiers of the Droplets
+     * @returns Promise
+     */
+    Firewalls.prototype.addDroplets = function (firewallId, dropletIds) {
+        return this._execute({
+            actionPath: this.basePath + "/" + encodeURIComponent(firewallId) + "/droplets",
+            method: common_1.HttpMethods.POST,
+            body: {
+                droplet_ids: dropletIds,
+            },
+        });
+    };
+    /**
+     * Remove Droplets from an existing Firewall
+     * @param firewallId the identifier of the Firewall to remove the Droplets from
+     * @param dropletIds the identifiers of the Droplets
+     * @returns Promise
+     */
+    Firewalls.prototype.removeDroplets = function (firewallId, dropletIds) {
+        return this._execute({
+            actionPath: this.basePath + "/" + encodeURIComponent(firewallId) + "/droplets",
+            method: common_1.HttpMethods.DELETE,
+            body: {
+                droplet_ids: dropletIds,
+            },
+        });
+    };
+    /**
+     * Add Tags to an existing Firewall
+     * @param firewallId the identifier of the Firewall to add the Tags to
+     * @param tags the Tags to add
+     * @returns Promise
+     */
+    Firewalls.prototype.addTags = function (firewallId, tags) {
+        return this._execute({
+            actionPath: this.basePath + "/" + encodeURIComponent(firewallId) + "/tags",
+            method: common_1.HttpMethods.POST,
+            body: {
+                tags: tags,
+            },
+        });
+    };
+    /**
+     * Remove Tags from an existing Firewall
+     * @param firewallId the identifier of the Firewall to remove the Tags from
+     * @param tags the Tags to remove
+     * @returns Promise
+     */
+    Firewalls.prototype.removeTags = function (firewallId, tags) {
+        return this._execute({
+            actionPath: this.basePath + "/" + encodeURIComponent(firewallId) + "/tags",
+            method: common_1.HttpMethods.DELETE,
+            body: {
+                tags: tags,
+            },
+        });
+    };
+    /**
+     * Add Rules to an existing Firewall
+     * @param firewallId the identifier of the Firewall to add the Rules to
+     * @param options the Rules to add
+     * @returns Promise
+     */
+    Firewalls.prototype.addRules = function (firewallId, options) {
+        return this._execute({
+            actionPath: this.basePath + "/" + encodeURIComponent(firewallId) + "/rules",
+            method: common_1.HttpMethods.POST,
+            body: options,
+        });
+    };
+    /**
+     * Remove Rules from an existing Firewall
+     * @param firewallId the identifier of the Firewall to remove the Rules from
+     * @param options the Rules to remove
+     * @returns Promise
+     */
+    Firewalls.prototype.removeRules = function (firewallId, options) {
+        return this._execute({
+            actionPath: this.basePath + "/" + encodeURIComponent(firewallId) + "/rules",
+            method: common_1.HttpMethods.DELETE,
+            body: options,
+        });
+    };
     return Firewalls;
 }(base_module_1.BaseModule));
 exports.default = Firewalls;
---
+++ src/types/firewalls.d.ts
@@ -0,0 +1,29 @@
+export interface FirewallChangeRequest {
+    name: string;
+    inbound_rules: FirewallInboundRule[];
+    outbound_rules: FirewallOutboundRule[];
+    droplet_ids: number[];
+    tags?: string[];
+}
+
+export interface FirewallInboundRule {
+    protocol: string;
+    ports: string;
+    sources: FirewallInboundRuleSource;
+}
+
+export interface FirewallInboundRuleSource {
+    load_balancer_uids?: string[];
+    tags?: string[];
+    addresses?: string[];
+}
+
+export interface FirewallOutboundRule {
+    protocol: string;
+    ports: string;
+    destinations: FirewallOutboundRuleDestination;
+}
+
+export interface FirewallOutboundRuleDestination {
+    addresses: string[];
+}
